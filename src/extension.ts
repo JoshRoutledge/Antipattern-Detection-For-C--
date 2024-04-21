0792df33b50552aac03305f415cf42bea531a28f
Josh Routledge <joshyrout@gmail.com>
2024-04-20 19:01:30 -0600

    Read me and clean up

--- src/extension.ts
+++ src/extension.ts
@@ -2,60 +2,45 @@ import { File } from 'buffer';
 import * as vscode from 'vscode';
 import { readdir, readFile } from 'fs/promises';
 import * as path from 'path';
-// import {CppFunction} from "./CppFunction";
 import {CppClass, CppFunction, Variable} from "./CppClass";
 import { availableParallelism } from 'os';
 import { assert } from 'console';
 import { maxHeaderSize } from 'http';
 
+
+//user settings
 let DEBUG = false;
-let ONLYT = true;
-const SAK_NUM_SETS = 2;
+const SAK_NUM_SETS = 2; //How many mutually exclusive sets of functions within a class are required to idetify a class as exhibiting the Swiss Army Knife antipattern
 const FE_THRESHOLD = 0.65;
+const TC_COMPLEX_THRESHOLD = 5; //How many operators are required to lable a conditional as "complex"
+const TC_NUMBER_CC_REQUIRED = 6; //How many complex conditionals are required to identify a class as exhibiting the TypeChecking antipattern
+const SPAGHETTI_LINES = 10; //How many lines are required for a function to be considered a "long" function
+const SPAGHETTI_FUNCS = 1; //How many long functions are required within a class to identify the class as exhibiting the Spaghetti Code antipattern
+
+//variables used to track certain aspects of the analyzed code
 let all_classes: CppClass[] = [];
 let classless_functions: CppFunction[] = [];
 let SAK_scores: number[] = [];
 let feature_envy_score: number[] = [];
 
-/*
-	Primatives :
-	"int",
-	"double",
-	"byte",
-	"short",
-	"char",
-	"long",
-	"float",
-	"boolean",
-	"void"
 
-	Collections:
-	Array<type>
-
-*/
-
-
-
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+/**
+ * Runs on activation of the extension adding our commands to users command palette
+ */
 export function activate(context: vscode.ExtensionContext) {
 
+	/**
+	 * A VS Code command that parses all c++ files in the active workspace into classes, functions, and variables
+	 * and the relationships between them.
+	 * 
+	 * This command is run upon calling `Parse Files` from the command palette.
+	 */
 	let disposable2 = vscode.commands.registerCommand('antitest.checkantipatterns', async () => {
 
-		// let primatives = [	"int",
-		// 					"double",
-		// 					"byte",
-		// 					"short",
-		// 					"char",
-		// 					"long",
-		// 					"float",
-		// 					"boolean",
-		// 					"void"];
-
-		// let collections = [ "array" ];
-
-		
+		all_classes = [];
+		classless_functions = [];
+		SAK_scores = [];
+		feature_envy_score = [];
 
 		function paramTypes(s:string){
 			var split_str = s.replace(/[^\w\,\s]/g, "").split(",");
@@ -75,6 +60,7 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Checking for common antipatterns.');
 		let moo = require('moo');
 	
+		// Define operators and delimiters
 		var opPat = [
 			// operators
 			'(',')', '[', ']', '{', '}',
@@ -83,15 +69,15 @@ export function activate(context: vscode.ExtensionContext) {
 			'&=','|=', '^=', '>>=', '<<=', '**=', '!',
 		  
 			// delimiters
-			'+','-', '**', '*', '//', '/', '%', // '@',
+			'+','-', '**', '*', '//', '/', '%',
 			'<<','>>', '<=', '>=', '==', '!=',
 			'&','|', '^', '~',
 			'<','>', '&&', '||',
 		  
-			// another operator
 			'=',
 		  ];
 
+		// Our lexer definition with the rules for tokenizing the input
 		let cpp_lexer = moo.compile({
 			WS:      {match: /\s+/, lineBreaks: true},
 			comment: [{match: /\/\/.*?$/},
@@ -121,6 +107,7 @@ export function activate(context: vscode.ExtensionContext) {
 			ERROR: moo.error,
 		});
 		
+		// Get the workspace folders and verify there is a only one selected workspace
 		const workspaceFolders = vscode.workspace.workspaceFolders;
 
 		if (!workspaceFolders || workspaceFolders.length === 0){
@@ -173,7 +160,7 @@ export function activate(context: vscode.ExtensionContext) {
 						DEBUG && console.log("Created class: " + token.value);
 						next();
 						if (token.type !== "lbracket") {
-							console.log("BAD NEWS BEARS NO LBRACKET AFTER CLASS");
+							console.log("Invalid Cpp");
 						}
 						break;
 
@@ -197,7 +184,7 @@ export function activate(context: vscode.ExtensionContext) {
 						}
 						else {
 							console.log(token);
-							console.log("Invalid Cpp me thinks");
+							console.log("Invalid Cpp");
 						}
 						break;
 
@@ -272,11 +259,9 @@ export function activate(context: vscode.ExtensionContext) {
 						break;
 					
 					case 'keyword':
-						//here we want can go through the work of parsing in the conditional if the keyword is if or while?
-						//maybe count the number of operators as a heuristic for the type-checking thingy
 						if (token.value === "if") {
 							next();
-							assert(token.value === "(", "if should be followed by (")
+							assert(token.value === "(", "if should be followed by (");
 							next();
 							let s = 0;
 							let paren_count = 1;
@@ -320,14 +305,21 @@ export function activate(context: vscode.ExtensionContext) {
 		
 	});
 
+	/**
+	 * A VS Code command that checks for common antipatterns in the parsed c++ files.
+	 * 
+	 * This command is run upon calling `Check for Antipatterns` from the command palette.
+	 */
 	let disposable1 = vscode.commands.registerCommand('antitest.checkclasses', async () => {
 
-		//placeholder may have a better location someday
+		SAK_scores = [];
+		feature_envy_score = [];
+
 		all_classes.forEach(c => {
 			c.functions.forEach(f => {
 				f.unknown_func_names.forEach(ufn => {
+					
 					//find correct function and add to f's functions
-
 					all_classes.forEach(c_fs => {
 						c_fs.functions.forEach(f_fs => {
 							if (ufn === f_fs.name) {
@@ -345,7 +337,6 @@ export function activate(context: vscode.ExtensionContext) {
 		divergent_change(all_classes, classless_functions);
 		shotgun_surgery(all_classes, classless_functions);
 		parallel_inheritance(all_classes, classless_functions);
-		functional_decomposition(all_classes, classless_functions);
 		spaghetti_code(all_classes, classless_functions);
 		swiss_army_knife(all_classes, classless_functions);
 		type_checking(all_classes, classless_functions);
@@ -356,7 +347,12 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable2);
 }
 
-
+/**
+ * Finds and returns all C++ file paths within a directory and its subdirectories 
+ * 
+ * @param dir root directory for search
+ * @returns Array of C++ file paths found
+ */
 async function readFilesInDirectory(dir: string): Promise<string[]> {
     let filesInDirectory = await readdir(dir, { withFileTypes: true });
     let files: string[] = [];
@@ -376,12 +372,15 @@ async function readFilesInDirectory(dir: string): Promise<string[]> {
     return files;
 }
 
+/**
+ * Checks for the presence of a god class in the provided classes and functions and prints to the console if found
+ * 
+ * A god class may be present if a class meets the thresholds for both Feature Envy and Swiss Army Knife.
+ * 
+ * @param classes Array of classes
+ * @param funcs Array of functions
+ */
  function god_class(classes: CppClass[], funcs:CppFunction[]){
-	/*
-		Checks for the presence of a god class.
-		A god class may be present if a class meets the thresholds for both
-		Feature Envy and Swiss Army Knife.
-	*/
 	let god_class_found = false;
 	for(let i = 0; i < all_classes.length; i++){
 		if (SAK_scores[i] > SAK_NUM_SETS) {
@@ -397,14 +396,18 @@ async function readFilesInDirectory(dir: string): Promise<string[]> {
 	}
  }
 
+/**
+ * Checks for the presence of feature envy in the provided classes and functions and prints to the console if found
+ * 
+ * Feature envy occurs when a method in one class relies too heavily on methods 
+ * from another class. 
+ * 
+ * Formula based on this research paper: https://ieeexplore.ieee.org/document/7051460
+ * 
+ * @param classes Array of classes
+ * @param funcs Array of functions
+ */
  function feature_envy(classes: CppClass[], funcs:CppFunction[]){
-	/*
-		Checks for feature envy between classes.
-		Feature envy occurs when a method in one class relies too heavily on methods 
-		from another class.
-
-		Formula based on this research paper: https://ieeexplore.ieee.org/document/7051460
-	*/
 	//Predefined threshhold weight: W and base: X
 	let W = 0.5;
 	let X = 0.5;
@@ -427,7 +430,6 @@ async function readFilesInDirectory(dir: string): Promise<string[]> {
 					var envy_factor = W * (M / total) + (1 - W) * (1 - (Math.pow(X, M)));
 					if(envy_factor > func_max){
 						func_max = envy_factor;
-						// DEBUG && console.log(`${func.name} envy factor increased to ${max}`);
 					}
 				});
 			}
@@ -443,63 +445,70 @@ async function readFilesInDirectory(dir: string): Promise<string[]> {
 	});
  }
 
+/**
+ * Checks for the presence of duplicate code in the provided classes and functions and prints to the console if found
+ * @param classes Array of classes
+ * @param funcs Array of functions
+ */
  function duplicate_code(classes: CppClass[], funcs:CppFunction[]){
 	DEBUG && console.log("duplicate code class not implemented");
-	//Doesn't use parser...
  }
 
+/**
+ * Checks for the presence of refused bequest in the provided classes and functions and prints to the console if found
+ * @param classes Array of classes
+ * @param funcs Array of functions
+ */
  function refused_bequest(classes: CppClass[], funcs:CppFunction[]){
 	DEBUG && console.log("refused bequest class not implemented");
-	//Inheritance.. oof
  }
 
+/**
+ * Checks for the presence of divergent change in the provided classes and functions and prints to the console if found
+ * @param classes Array of classes
+ * @param funcs Array of functions
+ */
  function divergent_change(classes: CppClass[], funcs:CppFunction[]){
 	DEBUG && console.log("diveregent change class not implemented");
-	//Difficult update parser
  }
 
+/**
+ * Checks for the presence of shotgun surgery in the provided classes and functions and prints to the console if found
+ * @param classes Array of classes
+ * @param funcs Array of functions
+ */
  function shotgun_surgery(classes: CppClass[], funcs:CppFunction[]){
 	DEBUG && console.log("shotgun surgery class not implemented");
-	//Difficult update parser
  }
 
+/**
+ * Checks for the presence of parallel inheritance in the provided classes and functions and prints to the console if found
+ * @param classes Array of classes
+ * @param funcs Array of functions
+ */
  function parallel_inheritance(classes: CppClass[], funcs:CppFunction[]){
 	DEBUG && console.log("parallel inher not implemented");
-	//inheritance
  }
 
- function functional_decomposition(classes: CppClass[], funcs:CppFunction[]){
-	DEBUG && console.log("functional decomp not implemented");
-
- }
-
+/**
+ * Checks for the presence of spaghetti code in the provided classes and functions and prints to the console if found
+ * @param classes Array of classes
+ * @param funcs Array of functions
+ */
  function spaghetti_code(classes: CppClass[], funcs:CppFunction[]){
-	// console.log("spaghetti code not implemented");
-	const SPAGHETTI_LINES = 10;
-	const SPAGHETTI_FUNCS = 1;
 
-	/*
-	Spaghetti code is revealed by classes with
-	no structure,
-	declaring long methods with no parameters,
-	utilizing global variables (this may need updates to the parser),
-	names of functions may suggest procedural programming (this would require too much work),
-	*/
-
-
-	//foreach class
 	classes.forEach(c => {
-		let n = 0;
+		let long_functions_count = 0;
 		c.functions.forEach(f => {
 			if (f.lines > SPAGHETTI_LINES) {
 				DEBUG && console.log(f.name + " has enough lines");
 				if (f.parameters.length === 0){
 					DEBUG && console.log(f.name + " has no parameters");
-					n = n + 1;
+					long_functions_count = long_functions_count + 1;
 				}
 			}
 		});
-		if (n >= SPAGHETTI_FUNCS) {
+		if (long_functions_count >= SPAGHETTI_FUNCS) {
 			console.log(c.name + " exhibits spaghetti code antipattern");
 		}
 		else {
@@ -508,31 +517,31 @@ async function readFilesInDirectory(dir: string): Promise<string[]> {
 	});
  }
 
+/**
+ * Checks for the presence of the swiss army knife antipattern in the provided classes and functions and prints to the console if found
+ * @param classes Array of classes
+ * @param funcs Array of functions
+ */
  function swiss_army_knife(classes: CppClass[], funcs:CppFunction[]){
-	//Easy - Peterson
-
-	//All classes start in their own "group"
-	//Define operator join, example if A-1 B-1 C-2 D-3 join C and A results in A,B,C-2 D-3
-	//Simple algorithm join C, A get A's group and set all values with that group to C's group
 
 	classes.forEach(c => {
 
-		let uj: number[] = [];
+		let groups: number[] = []; //Used to track which "set" each class belogs to
 		for (let i = 0; i < classes.length; i++) {
-			uj.push(-1);
+			groups.push(-1); //initialize each class to be in the set that doesn't call any functions within c
 		}
 
 		c.functions.forEach(f => {
 			let classesThatCallF: number[] = [];
 
-			//find all the classes that call this function join them
+			//find all the classes that call f
 			for (let j = 0; j < classes.length; j++) {
-				const element = classes[j];
-				for (let k = 0; k < element.functions.length; k++) {
-					const t = element.functions[k];
-					t.func_calls.forEach(e => {
-						//e is a function call from class j to function e
-						if (e.name === f.name) {
+				const classJ = classes[j];
+				for (let k = 0; k < classJ.functions.length; k++) {
+					const functionK = classJ.functions[k];
+					functionK.func_calls.forEach(jCall => {
+						//jCall is a function call from class j's function k to jCall
+						if (jCall.name === f.name) { //is this function call calling f which is in c
 							//we have found that class j calls function f track it
 							classesThatCallF.push(j);
 						}
@@ -541,28 +550,28 @@ async function readFilesInDirectory(dir: string): Promise<string[]> {
 				
 			}
 
-			//we now have all classes that call F they must be in the same group, join them
+			//join the classes that call f into the same group
 			for (let l = 0; l < classesThatCallF.length; l++) {
 				const element = classesThatCallF[l];
-				let a = classesThatCallF[0]; //index into uj
-				let b = classesThatCallF[l]; //index into uj
+				let a = classesThatCallF[0]; //index into group arbitrarilly select a's group as the group to join into
+				let b = classesThatCallF[l]; //index into group arbitrarilly select b's group as the group to join from
 
-				//join a and b
-				let a_value = uj[a];
-				let b_value = uj[b];
+				let a_value = groups[a];
+				let b_value = groups[b];
 
-				if (a_value === -1) {
+				if (a_value === -1) { // is a's group the group that doesn't call c
 					a_value = a;
-					uj[a] === a_value;
+					groups[a] === a_value; //if so give a a "new" group as it calls f
 				}
-				if (b_value === -1) {
+				if (b_value === -1) { // is b's current group the group that doesn't call c
 					b_value = b;
-					uj[b] = b_value;
+					groups[b] = b_value; // if so give b a "new" group, as it calls f
 				}
 
-				for (let m = 0; m < uj.length; m++) {
-					if (uj[m] === b_value) {
-						uj[m] = a_value;
+				//set all values in b's group to a's group
+				for (let m = 0; m < groups.length; m++) {
+					if (groups[m] === b_value) {
+						groups[m] = a_value;
 					}
 				}
 				
@@ -570,29 +579,32 @@ async function readFilesInDirectory(dir: string): Promise<string[]> {
 		});
 		
 		//here we determine if c is a sak
-		const s = (new Set(uj)).size - 1; //used to count how many "groups" not counting the no call group
+		const s = (new Set(groups)).size - 1; //used to count how many "groups" not counting the no call group
 		if (s >= SAK_NUM_SETS) {
 			console.log(c.name + " may be a Swiss Army Knife");
 		}
-		SAK_scores.push(s);
+		SAK_scores.push(s); //track score for the Godclass identifier
 	});
-
-	//Now we should have the number of groups
 
  }
 
+/**
+ * Checks for the presence of type checking in the provided classes and functions and prints to the console if found
+ * @param classes Array of classes
+ * @param funcs Array of functions
+ */
  function type_checking(classes: CppClass[], funcs:CppFunction[]){
-	const TC_COMPLEX_THRESHOLD = 5;
-	const TC_NUMBER_CC_REQUIRED = 6;
 
-	let count = 0;
+	let cc_count = 0;
 	classes.forEach(c => {
-		c.conditionals.forEach(pcc => {
-			if (pcc >= TC_COMPLEX_THRESHOLD) {
-				count = count + 1;
+		c.conditionals.forEach(potential_complex_conditional => {
+			if (potential_complex_conditional >= TC_COMPLEX_THRESHOLD) {
+				cc_count = cc_count + 1;
 			}
 		});
-		if (count >= TC_NUMBER_CC_REQUIRED) {
+
+
+		if (cc_count >= TC_NUMBER_CC_REQUIRED) {
 			console.log(c.name + " is an example of a type checking antipattern");
 		}
 	});
